c05144358a576a31221b9e7caec17f7f2c34e64e
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-03-16 16:10:03 -0600

    Sign in with LinkedIn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   # You can have the root of your site routed with "root"
   root 'users#welcome'
-  get '/auth/linkedin/callback' => 'users#show'
+  match "/auth/:provider/callback" => "sessions#create", via: [:get, :post]
+  match "/signout" => "sessions#destroy", as: 'signout', via: [:get, :post]
 
 end
