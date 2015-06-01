0e4bfbcebf5da433e959feab520c49013ba56e75
Daniel Harris <bobth3bum@gmail.com>
2015-06-01 13:37:20 -0600

    goodbye 80 man hours

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV['LINKEDIN_API_KEY'], ENV['LINKEDIN_SECRET_KEY'], :scope => 'r_fullprofile r_emailaddress r_contactinfo'
+  provider :linkedin, ENV['LINKEDIN_API_KEY'], ENV['LINKEDIN_SECRET_KEY'], :scope => 'r_fullprofile r_emailaddress'
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
